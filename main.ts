f0cd3c57b6f8ca4787f53700e3f06fd9d68f6434
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2021-11-22 21:04:39 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    eureka_plotter_car.plottercar_zengo(plotter_houkou.前, 1)
-})
-eureka_plotter_car.microbit_version_info(microbit_version.Version2)
-music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
 basic.forever(function () {
 	
 })
